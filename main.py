c765e4dcf56dfdd7eee8ca29a8415607996b2e8e
RAFAELARTUROGUTIERRE <rafael.gutierrez4666@alumnos.udg.mx>
2024-12-07 04:22:33 -0600

    advance with the project: add csv and configuring paging

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="letter")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Arial",style="B",size=12)
-pdf.cell(w=0,h=12,txt="HelloWorld",align="l",ln=1,border=1)
-
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Arial",style="B",size=24)
+    pdf.set_text_color(100,100,100)
+    topic = row["Topic"]
+    pdf.cell(w=0,h=12,txt=topic,align="L",ln=1)
+    pdf.line(10,23,200,23)
 pdf.output("output.pdf")
